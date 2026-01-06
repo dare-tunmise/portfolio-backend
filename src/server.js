8438826d2f38efef7c6338479a5ef81a656e09cc
Dare Tunmise <daretunmise@yahoo.com>
2026-01-06 20:54:13 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:3000','http://localhost:3000/', 'https://daretunmise.com', 'https://www.daretunmise.com'],
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000','http://localhost:3000/', 'https://daretunmisee.onrender.com', 'https://daretunmise.com', 'https://www.daretunmise.com'],
   credentials: true
 }));
 
